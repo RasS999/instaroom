c668b9cd62f47b3d8f807ee8558b9e064e8e667e
Ras D.P. Sanchez <ras.sanchez80@gmail.com>
2025-04-07 18:35:47 +0800

    Initial commit

--- app/app.js
+++ app/app.js
@@ -81,4 +81,4 @@ app.use('/', viewRoutes);  // Main route for views (without '/views' prefix)
 const PORT = 5001;
 app.listen(PORT, () => {
     console.log(`Server running on http://127.0.0.1:${PORT}`);
-}); 
+});
